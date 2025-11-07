8474b6286c161640e6c62caffcd1ac847bb29cde
Mahens3 <razakatiambolaphillipe@gmail.com>
2025-11-07 09:25:50 +0300

    Maps initialisation

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,8 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
+import 'leaflet/dist/leaflet.css';
+
 
 import AppRouter from "./routes/AppRouter.tsx";
 import { AuthProvider } from "./contexts/AuthContext.tsx";
